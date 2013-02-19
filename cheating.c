3a165122ad66b54d19ab23eeee933ee7bdec2b14
Gavin Higham <Gavin.Higham@gmail.com>
2013-02-18 19:13:38 -0800

    Sharing a thing with Keenan.

---
+++ cheating.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void swap(int* a, int* b)
+{
+	printf("%i was swapped with %i.\n", *a, *b);
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int partition(int a[], int p, int r)
+{
+	int j;
+	int z;
+	int x = a[r];
+	int i = p-1;
+	for (j = p; j < r; j++) {
+		if (a[j] <= x) {
+			i++;
+			swap(&a[i], &a[j]);
+			printf("When j is %2i, array is: ", j);
+			for (z = 0; z < r; z++) printf("[%i]", a[z]);
+			printf("\n");
+		}
+	}
+	swap(&a[i+1], &a[r]);
+	return i+1;
+}
+
+int main()
+{
+	int array[] = {13, 19, 9, 5, 12, 8, 7, 4, 21, 2, 6, 11};
+	//int array[] = {2, 8, 7, 1, 3, 5, 6, 4};
+	printf("i is: %i\n", partition(array, 0, sizeof(array)/sizeof(int)));
+	return 0;
+}
