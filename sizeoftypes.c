65f5b7db0fe2567de6c6df9cb65e327ddb638bb7
Gavin Higham <Gavin.Higham@gmail.com>
2013-02-25 13:08:33 -0800

    Got rid of duplicate files from the repo\!

--- sizeoftypes.c
+++
@@ -1,8 +0,0 @@
-#import <stdio.h>
-
-//Prints the size of various C types. I'll periodically add more types as I need to know them.
-
-int main() {
-	printf("Size of a long long int: %lu\n", sizeof(long long unsigned int));
-	return 0;
-}
